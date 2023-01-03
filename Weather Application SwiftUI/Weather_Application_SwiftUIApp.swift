7b98ba63124b9bf8df8d617593a333378bfb6d0d
sahidcse15@gmail.com <sahidcse15@gmail.com>
2023-01-03 10:20:53 +0530

    Initial Commit

---
+++ Weather Application SwiftUI/Weather_Application_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_Application_SwiftUIApp.swift
+//  Weather Application SwiftUI
+//
+//  Created by Sahid Reza on 03/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_Application_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
